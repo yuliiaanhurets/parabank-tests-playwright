0307db0a06e3077c0b0b1aa8d166960f23f7e4ef
yuliiaanhurets <yangurets@gmail.com>
2024-04-01 12:48:22 +0300

    Added fixtures

--- pages/homePage.ts
+++ pages/homePage.ts
@@ -6,15 +6,31 @@ export class HomePage {
     readonly atmServices: Locator;
     readonly onlineServices: Locator;
     readonly readMoreLink: Locator;
+    readonly userNameField: Locator;
+    readonly passwordField: Locator;
+    readonly submitButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.title = page.locator('//span[@class="title"]');
         this.atmServices = page.locator('.services');
         this.onlineServices = page.locator('.servicestwo');
+        this.userNameField = page.locator('//input[@name="username"]');
+        this.passwordField = page.locator('//input[@name="password"]');
+        this.submitButton = page.locator('//input[@type="submit"]');
     }
 
     async clickOnReadMoreLink() {
         await this.page.getByText('Read More').first().click();
+    }
+
+    async goTo() {
+        await this.page.goto('https://parabank.parasoft.com/parabank/index.htm');
+    }
+
+    async login(username: string, password: string) {
+        await this.userNameField.fill(username);
+        await this.passwordField.fill(password);
+        await this.submitButton.click();
     }
 }
